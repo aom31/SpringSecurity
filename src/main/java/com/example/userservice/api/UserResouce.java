afd242e63dcdefbf95500d6edf7dcd62a7b75dfc
aom31 <97218117+aom31@users.noreply.github.com>
2022-06-20 02:44:17 +0700

    controller

--- src/main/java/com/example/userservice/api/UserResouce.java
+++ src/main/java/com/example/userservice/api/UserResouce.java
@@ -1,14 +1,15 @@
 package com.example.userservice.api;
 
+import com.example.userservice.domain.Role;
 import com.example.userservice.domain.User;
 import com.example.userservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,6 +23,17 @@ public class UserResouce {
     @GetMapping("/alluser")
     public ResponseEntity<List<User>> getUser(){
         return ResponseEntity.ok().body(userService.getUsers());
+    }
+
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        return ResponseEntity.ok().body(userService.saveRole(role));
     }
 
 
